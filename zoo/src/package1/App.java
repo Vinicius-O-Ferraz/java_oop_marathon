c0a56687278046d9f7bfb342fd1a825e1ec26eaa
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2024-10-27 13:30:40 -0300

    Mock implementation of the Interface

--- zoo/src/package1/App.java
+++ zoo/src/package1/App.java
@@ -5,21 +5,47 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String[] nomes;
-/*        System.out.println("Welcome to the Zoo!");
-
-        Animal lion = new Animal("Lion Alva",0,176.5f,"Carnivore", 'M',new Date());
-        lion.info();
-        lion.reproduce();*/
 
         Animal[] animals = new Animal[100];
         Scanner sc = new Scanner(System.in);
 
         int controller = 1 ;
         while(controller!=0){
+            menu();
             controller = sc.nextInt();
+
+            if (controller==1){
+                System.out.println("Creating animals");
+            }
+
+            else if (controller==2){
+                System.out.println("Reading animais");
+                for (int i = 0; i < animals.length; i++) {
+                    System.out.println(animals[i]);
+                }
+            }
+
+            else if (controller==3){
+                System.out.println("Updating animals");
+            }
+
+            else if (controller==4){
+                System.out.println("Deleting animais");
+            }
         }
+        sc.close();
         System.out.println("End of program");
+    }
+
+    public static void menu(){
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        System.out.println("Welcome to the Zoo Menu");
+        System.out.println("1. Create Animal");
+        System.out.println("2. Reading Animal");
+        System.out.println("3. Updating Animal");
+        System.out.println("4. Deleting Animal");
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
     }
 }
 
