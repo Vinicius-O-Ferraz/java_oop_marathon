26ddf787be9d32b29c73ee1ebdd917ae4a0e020e
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2024-10-27 12:43:31 -0300

    Implementing AquaticAnimals e LandAnimals

--- zoo/src/package1/AquaticAnimals.java
+++ zoo/src/package1/AquaticAnimals.java
@@ -1,4 +1,58 @@
 package package1;
 
-public class AquaticAnimals extends Invertebrate{
+public class AquaticAnimals extends Animal {
+    // Atributos específicos da classe AquaticAnimal
+    private String waterType;            // Tipo de água: "Doce" ou "Salgada"
+    private boolean canBreatheUnderwater; // Indica se o animal pode respirar debaixo d'água
+
+    // Construtor da classe filha
+    public AquaticAnimals(
+            String animal_name,
+            int animal_id,
+            float weight,
+            String diet,
+            char sex,
+            java.util.Date birth_Date,
+            String waterType,
+            boolean canBreatheUnderwater
+    ) {
+        // Chama o construtor da classe pai
+        super(animal_name, animal_id, weight, diet, sex, birth_Date);
+        this.waterType = waterType;
+        this.canBreatheUnderwater = canBreatheUnderwater;
+    }
+
+    // Getters e Setters para os novos atributos
+    public String getWaterType() {
+        return waterType;
+    }
+
+    public void setWaterType(String waterType) {
+        this.waterType = waterType;
+    }
+
+    public boolean isCanBreatheUnderwater() {
+        return canBreatheUnderwater;
+    }
+
+    public void setCanBreatheUnderwater(boolean canBreatheUnderwater) {
+        this.canBreatheUnderwater = canBreatheUnderwater;
+    }
+
+    // Métodos específicos da classe AquaticAnimal
+    public void swim() {
+        System.out.println("The aquatic animal is swimming.");
+    }
+
+    public void dive() {
+        System.out.println("The aquatic animal is diving deep.");
+    }
+
+    // Sobrescrita (Override) do método info da classe Animal
+    @Override
+    public void info() {
+        super.info(); // Chama o método info da classe pai
+        System.out.println("Water Type: " + getWaterType());
+        System.out.println("Can Breathe Underwater: " + (isCanBreatheUnderwater() ? "Yes" : "No"));
+    }
 }
--- zoo/src/package1/LandAnimals.java
+++ zoo/src/package1/LandAnimals.java
@@ -1,5 +1,60 @@
 package package1;
 
-public class LandAnimals extends Invertebrate{
+public class LandAnimals extends Invertebrate {
+    // Atributos específicos da classe LandAnimal
+    private String terrainType;   // Tipo de terreno: "Florestal", "Desértico", etc.
+    private boolean isNocturnal;  // Indica se o animal é noturno
 
+    // Construtor da classe filha
+    public LandAnimals(
+            String animal_name,
+            int animal_id,
+            float weight,
+            String diet,
+            char sex,
+            java.util.Date birth_Date,
+            String habitat,
+            boolean canRegenerate,
+            String terrainType,
+            boolean isNocturnal
+    ) {
+        // Chama o construtor da classe Invertebrate
+        super(animal_name, animal_id, weight, diet, sex, birth_Date, habitat, canRegenerate);
+        this.terrainType = terrainType;
+        this.isNocturnal = isNocturnal;
+    }
+
+    // Getters e Setters para os novos atributos
+    public String getTerrainType() {
+        return terrainType;
+    }
+
+    public void setTerrainType(String terrainType) {
+        this.terrainType = terrainType;
+    }
+
+    public boolean isNocturnal() {
+        return isNocturnal;
+    }
+
+    public void setNocturnal(boolean nocturnal) {
+        isNocturnal = nocturnal;
+    }
+
+    // Métodos específicos da classe LandAnimal
+    public void run() {
+        System.out.println("The land animal is running swiftly.");
+    }
+
+    public void climb() {
+        System.out.println("The land animal is climbing a tree.");
+    }
+
+    // Sobrescrita (Override) do método info da classe Invertebrate
+    @Override
+    public void info() {
+        super.info(); // Chama o método info da classe Invertebrate
+        System.out.println("Terrain Type: " + getTerrainType());
+        System.out.println("Is Nocturnal: " + (isNocturnal() ? "Yes" : "No"));
+    }
 }
